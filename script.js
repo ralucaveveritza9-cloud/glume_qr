83597e350ebe1f926f5b5f68729754dc222f0065
ralucaveveritza9-cloud <ralucaveveritza9@gmail.com>
2026-07-24 13:43:40 +0300

    Adaug logica jocului

---
+++ script.js
@@ -0,0 +1,97 @@
+let glume = [];
+let provocari = [];
+let jackpot = [];
+
+const sloturi = document.querySelectorAll(".slot");
+const categorie = document.getElementById("categorie");
+const mesaj = document.getElementById("mesaj");
+const buton = document.getElementById("startBtn");
+
+const simboluri = ["🍒","🍋","⭐","7️⃣","💎","🍀","🔔","❤️"];
+
+async function incarca() {
+    try{
+        glume = await fetch("glume.json").then(r=>r.json());
+        provocari = await fetch("provocari.json").then(r=>r.json());
+        jackpot = await fetch("jackpot.json").then(r=>r.json());
+    }catch(e){
+        mesaj.textContent="Nu pot încărca fișierele JSON.";
+    }
+}
+
+function random(lista){
+    return lista[Math.floor(Math.random()*lista.length)];
+}
+
+async function spin(){
+
+    buton.disabled=true;
+
+    categorie.textContent="🎰 SE ÎNVÂRTE...";
+    mesaj.textContent="";
+
+    const animatie=setInterval(()=>{
+
+        sloturi.forEach(slot=>{
+            slot.classList.add("slot-spin");
+            slot.textContent=random(simboluri);
+        });
+
+    },100);
+
+    await new Promise(r=>setTimeout(r,2000));
+
+    clearInterval(animatie);
+
+    sloturi.forEach(slot=>slot.classList.remove("slot-spin"));
+
+    let rezultat;
+    let sansa=Math.random();
+
+    if(sansa<0.08 && jackpot.length){
+
+        categorie.textContent="👑 JACKPOT";
+
+        rezultat=random(jackpot);
+
+        confetti({
+            particleCount:250,
+            spread:120,
+            origin:{y:0.6}
+        });
+
+    }
+
+    else if(sansa<0.55 && provocari.length){
+
+        categorie.textContent="🎯 PROVOCARE";
+
+        rezultat=random(provocari);
+
+        confetti({
+            particleCount:120,
+            spread:90
+        });
+
+    }
+
+    else{
+
+        categorie.textContent="😂 GLUMĂ";
+
+        rezultat=random(glume);
+
+    }
+
+    mesaj.textContent =
+        typeof rezultat==="string"
+        ? rezultat
+        : rezultat.text;
+
+    buton.disabled=false;
+
+}
+
+buton.addEventListener("click",spin);
+
+incarca();
